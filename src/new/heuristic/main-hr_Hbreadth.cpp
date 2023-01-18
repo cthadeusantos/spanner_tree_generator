4dd7fcb3fb6efad14375e00592a8539356fe0c2a
cthadeusantos <cthadeusantos@gmail.com>
2023-01-18 16:42:37 -0300

    Fixed breadth heuristic main Now setting breadth heuristic not H2v2

--- src/new/heuristic/main-hr_Hbreadth.cpp
+++ src/new/heuristic/main-hr_Hbreadth.cpp
@@ -153,9 +153,9 @@ int main(int argc, char** argv){
 	std::chrono::time_point<std::chrono::steady_clock> start = std::chrono::steady_clock::now();
 
 	// MAIN PROCEDURE
-	DEBUG std::cerr << "Solving with heuristic 2 vertex importance- wait!\n";
-	run_name = "Heuristic_2v2";
-	Heuristic::heuristica_2_modified(graph);
+	DEBUG std::cerr << "Solving with breadth heuristic - vertex importance- wait!\n";
+	run_name = "Breadth_heuristic";
+	Heuristic::breadth_heuristic(graph);
 
 	// End time counting
 	std::chrono::time_point<std::chrono::steady_clock>	end = std::chrono::steady_clock::now();	
