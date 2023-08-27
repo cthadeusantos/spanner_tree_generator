6b0be46cdf0ad676d90926edd2638bff56f2b173
cthadeusantos <cthadeusantos@gmail.com>
2023-08-27 18:15:44 -0300

    new dataset, battery to count trees, bash scripts added

---
+++ tools/execute_batteries-grafos-one-hour.sh
@@ -0,0 +1,4 @@
+python3 execution-battery.py 1 < batteries/04-one-hour-EDGE.yaml
+python3 execution-battery.py 1 < batteries/04-one-hour-CYCLE-edge.yaml
+python3 execution-battery.py 1 < batteries/04-one-hour-ADJACENCY.yaml
+python3 execution-battery.py 1 < batteries/04-one-hour-SEQ.yaml
